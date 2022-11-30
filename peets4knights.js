e1aa2821557154b40706fc533bc8f8c3c51b55a0
Ben Tanis <bentanis@dhcp96-121.calvin.edu>
2022-11-30 16:32:07 -0500

    ItemType2

--- peets4knights.js
+++ peets4knights.js
@@ -46,7 +46,7 @@ function readHelloMessage(req, res) {
 }
 
 function readitemInfo(req, res, next) {
-  db.many("SELECT * FROM itemInfo WHERE itemtype=${itemtype}")
+  db.many("SELECT * FROM itemInfo WHERE itemtype = ${body.itemtype}")
     .then(data => {
       res.send(data);
     })
